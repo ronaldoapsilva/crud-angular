e125a849b507ff0cfb7e54cfacb518e9d54d48a6
Ronaldo Aparecido da Silva <ronaldoapsilva_@hotmail.com>
2024-06-15 02:08:03 -0300

    07 - CSS do Material Table e Criando um Módulo App Material

--- src/app/courses/courses.module.ts
+++ src/app/courses/courses.module.ts
@@ -1,18 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { MatTableModule } from '@angular/material/table';
 
+import { AppMaterialModule } from '../shared/app-material/app-material.module';
 import { CoursesRoutingModule } from './courses-routing.module';
 import { CoursesComponent } from './courses/courses.component';
 
+
 @NgModule({
   declarations: [
-    CoursesComponent
+    CoursesComponent,
+
   ],
   imports: [
     CommonModule,
     CoursesRoutingModule,
-    MatTableModule
+    AppMaterialModule
   ]
 })
 export class CoursesModule { }
